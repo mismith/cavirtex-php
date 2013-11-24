41ad72b122f85313503fb702a4f4a352e400e036
Murray Smith <murray@mismith.info>
2013-11-24 15:51:32 -0700

    added wordpress test site

--- test/ipn.php
+++ test/ipn.php
@@ -1,5 +1,13 @@
 <?php
 
+// setup the API
+require_once('cavirtex.config.php');
 require_once('../cavirtex_merchant_api.php');
 
-Cavirtex_Merchant_Api::process_ipn();
+$cavirtex = new Cavirtex_Merchant_Api(CAVIRTEX_MERCHANT_KEY, CAVIRTEX_MERCHANT_SECRET);
+
+$payloadString = var_export($cavirtex->process_ipn(), TRUE) . "\n";
+
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/ipn.log', $payloadString, FILE_APPEND);
+
+echo $payloadString;
